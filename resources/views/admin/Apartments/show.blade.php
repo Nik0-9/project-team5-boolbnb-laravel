beec4d14c73d01c2afafcff09ddd56aaf6f34f00
adrisala20 <adriana_15_tuya@hotmail.com>
2024-07-01 18:13:30 +0200

    added edit button in apartment show

--- resources/views/admin/Apartments/show.blade.php
+++ resources/views/admin/Apartments/show.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
-@section('content')
-    
-    <h1>Dettagli Appartamento</h1>
-    <p><strong>Nome:</strong> {{ $apartment->name }}</p>
-    <img class="w-50" src="{{ asset('storage/'.$apartment->cover_image)}}" alt="{{ $apartment->name }}">
-    <p><strong>Indirizzo:</strong> {{ $apartment->address }}</p>
-    <p><strong>Descrizione:</strong> {{ $apartment->description }}</p>
-    <a href="{{ route('admin.apartments.index') }}" class="btn btn-secondary">Torna alla Lista</a>
+@section('content') 
+<div class="card m-4 border-0">
+        <h1 class="d-inline">Dettagli Appartamento</h1>
+        <div class="d-flex justify-content-end" >
+        <a href="{{route('admin.apartments.edit', $apartment->slug)}}" class="btn btn-success ">Edit</a>
+        </div>
+        <p><strong>Nome:</strong> {{ $apartment->name }}</p> 
+        <img class="w-50 m-auto pb-3" src="{{ $apartment->cover_image }}" alt="{{ $apartment->name }}">
+        <p><strong>Indirizzo:</strong> {{ $apartment->address }}</p>
+        <p><strong>Descrizione:</strong> {{ $apartment->description }}</p>
+        <a href="{{ route('admin.apartments.index') }}" class="btn btn-secondary mx-2">Torna alla Lista</a>
+    </div>
 @endsection
