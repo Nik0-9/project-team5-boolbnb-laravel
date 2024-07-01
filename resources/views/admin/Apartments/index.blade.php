dbefe27a1f59eaf56bd09daeb034ca2006e062ad
DavideAlovisio13 <alovisiod@gmail.com>
2024-07-01 13:11:47 +0200

    create new dashboard and insert charts.js

--- resources/views/admin/Apartments/index.blade.php
+++ resources/views/admin/Apartments/index.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Appartamenti</h1>
+    <!-- Begin Page Content -->
+   <h1>Appartamenti</h1>
     <a href="{{ route('admin.apartments.create') }}" class="btn btn-primary">Crea Nuovo Appartamento</a>
     <table class="table">
         <thead>
@@ -13,18 +14,22 @@
             </tr>
         </thead>
         <tbody>
-            @foreach($apartments as $apartment)
+            @foreach ($apartments as $apartment)
                 <tr>
                     <td>{{ $apartment->id }}</td>
                     <td>{{ $apartment->name }}</td>
                     <td>{{ $apartment->address }}</td>
                     <td>
-                        <a href="{{ route('admin.apartments.show', $apartment->slug) }}" class="btn btn-info" id="btnInfo"><i class="fas fa-eye"></i></a>
-                        <a href="{{ route('admin.apartments.edit', $apartment->slug) }}" class="btn btn-warning"><i class="fas fa-pencil-alt"></i></a>
-                        <form action="{{ route('admin.apartments.destroy', $apartment->slug) }}" method="POST" style="display:inline;">
+                        <a href="{{ route('admin.apartments.show', $apartment->slug) }}" class="btn btn-info" id="btnInfo"><i
+                                class="fas fa-eye"></i></a>
+                        <a href="{{ route('admin.apartments.edit', $apartment->slug) }}" class="btn btn-warning"><i
+                                class="fas fa-pencil-alt"></i></a>
+                        <form action="{{ route('admin.apartments.destroy', $apartment->slug) }}" method="POST"
+                            style="display:inline;">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="btn btn-danger delete-button"><i><i class="fas fa-trash-alt"></i></i></button>
+                            <button type="submit" class="btn btn-danger delete-button"><i><i
+                                        class="fas fa-trash-alt"></i></i></button>
                         </form>
                     </td>
                 </tr>
