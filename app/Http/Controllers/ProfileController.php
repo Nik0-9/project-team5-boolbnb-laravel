4e5a8997178f017cead29a4e07528f91fa855137
Nik0-9 <filannino.nicola9@gmail.com>
2024-07-02 10:12:07 +0200

    fix minor

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -55,6 +55,6 @@ class ProfileController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return Redirect::to('http://localhost:5174');
+        return Redirect::to('http://localhost:8000');
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -153,7 +153,7 @@
                    {{-- <div class="card  mb-4">
                         <div class="card-header py-3">
                             <h6 class="m-0 font-weight-bold ">...</h6>
-                            {{-- <a href="{{ route('admin.movies.index') }}" class="">Mostra tutti i film</a> --}}
+                            {{-- <a href="{{ route('admin.movies.index') }}" class="">Mostra tutti i film</a>
                         </div>
                         <div class="card-body">
                              @foreach ($movies as $movie)
@@ -168,7 +168,7 @@
 
                                 </div>
                             </div>
-                        @endforeach --}}
+                        @endforeach --}} 
                         </div>
                     </div>
                 </div>
