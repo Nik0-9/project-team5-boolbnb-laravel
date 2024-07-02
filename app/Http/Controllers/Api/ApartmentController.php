4c37987abceced6fba158ebd4d4f4baec0f6e238
DavideAlovisio13 <alovisiod@gmail.com>
2024-07-02 14:04:27 +0200

    routes for api

---
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+use App\Models\Apartment;
+use Illuminate\Http\Request;
+
+class ApartmentController extends Controller
+{
+    public function index()
+    {
+        $data = Apartment::with('images', 'services')->get();
+        return response()->json([
+            'success' => true,
+            'results' => $data
+    ]);
+
+    }
+
+    public function store(Request $request)
+    {
+        $item = Apartment::create($request->all());
+        return response()->json($item, 201);
+    }
+
+    public function show($id)
+    {
+        return Apartment::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $item = Apartment::findOrFail($id);
+        $item->update($request->all());
+        return response()->json($item, 200);
+    }
+
+    public function destroy($id)
+    {
+        Apartment::destroy($id);
+        return response()->json(null, 204);
+    }
+}
+
+
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Api\ApartmentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,9 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('apartments', [\App\Http\Controllers\Api\ApartmentController::class, 'index'])->name('apartments.index');
+Route::get('apartment/{id}', [\App\Http\Controllers\Api\ApartmentController::class, 'show'])->name('apartments.show');
+Route::get('apartment/{slug}/services', [\App\Http\Controllers\Api\ApartmentController::class, 'services'])->name('apartments.services');
+
+
