e8323144e8117cf0d5c5797f3080c8c18d375cd0
Nik0-9 <filannino.nicola9@gmail.com>
2024-07-04 13:19:45 +0200

    function search api controller

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -25,6 +25,26 @@ class ApartmentController extends Controller
             'results' => $sponsoredApartments
         ]);
     }
+    public function search($address, $lat, $lon)
+    {
+        // Converti lat e lon da gradi a radianti
+        $lat = deg2rad($lat);
+        $lon = deg2rad($lon);
+
+        // Raggio in km (20 km)
+        $radius = 20;
+
+        // Raggio in metri (20 km convertito in metri)
+        $radiusMeters = $radius * 1000;
+
+        // Query per trovare gli appartamenti entro il raggio specificato
+        $apartments = Apartment::select('*')
+            ->whereRaw("6371 * acos(cos(radians($lat)) * cos(radians(lat)) * cos(radians(lon) - radians($lon)) + sin(radians($lat)) * sin(radians(lat))) <= $radius")
+            ->get();
+
+        // Esempio di output
+        return response()->json(['apartments' => $apartments]);
+    }
 
     public function store(Request $request)
     {
