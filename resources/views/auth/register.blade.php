ea16f9400461d3ade7cee7e0bb6d9d2c19f13f3c
Nik0-9 <filannino.nicola9@gmail.com>
2024-07-05 15:50:24 +0200

    fix email validation in register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,8 @@
                 <div class="card-header">{{ __('Registrati') }}</div>
 
                 <div class="card-body">
-                    <form id="register-form" method="POST" action="{{ route('register') }}">
+                    <form id="register-form" method="POST" action="{{ route('register') }}"
+                        onsubmit="validateEmail(event)">
                         @csrf
 
                         <div class="mb-4 row">
@@ -17,13 +18,11 @@
                             <div class="mb-4 row">
                                 <label for="email"
                                     class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo mail') }} *</label>
-
                                 <div class="col-md-6">
-
-                                <input type="email" id="email" pattern="^[a-zA-Z][a-zA-Z0-9._%+-]*@[a-zA-Z]{3,}\.[a-zA-Z]{2,}$" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" required />
-
-                                    <div id="email-feedback" class="invalid-feedback">
-                                        Inserire una email valida
+                                    <input type="email" id="email" name="email" class="form-control"
+                                        pattern="^[a-z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$" required>
+                                    <div id="emailError" class="text-danger d-none" role="alert">
+                                        <strong>Indirizzo email non valido: <br>esempio@email.com</strong>
                                     </div>
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
@@ -33,6 +32,7 @@
                                 </div>
                             </div>
 
+
                             <div class="mb-4 row">
                                 <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}
                                     *</label>
@@ -40,7 +40,7 @@
                                 <div class="col-md-6">
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="new-password">
+                                        required>
 
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
@@ -57,7 +57,7 @@
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control"
-                                        name="password_confirmation" required autocomplete="new-password">
+                                        name="password_confirmation" required>
                                     <span id="password-match-error" class="invalid-feedback" role="alert"
                                         style="display: none;">
                                         <strong>Le password non corrispondono.</strong>
@@ -65,63 +65,66 @@
                                 </div>
                             </div>
 
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
+                            <div class="mb-4 row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
+                                <div class="col-md-6">
+                                    <input id="name" type="text" pattern="[a-zA-Z\s]+" title="Solo lettere sono ammesse"
+                                        class="form-control @error('name') is-invalid @enderror" name="name"
+                                        value="{{ old('name') }}" autofocus>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" pattern="[a-zA-Z\s]+" title="Solo lettere sono ammesse"
-                                    class="form-control @error('name') is-invalid @enderror" name="name"
-                                    value="{{ old('name') }}" autocomplete="name" autofocus>
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+
+                            <div class="mb-4 row">
+                                <label for="surname"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Cognome') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="surname" type="text" pattern="[a-zA-Z\s]+"
+                                        title="Solo lettere sono ammesse"
+                                        class="form-control @error('surname') is-invalid @enderror" name="surname"
+                                        value="{{ old('surname') }}" autofocus>
+
+                                    @error('surname')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="mb-4 row">
+                                <label for="date_of_birth"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Data di nascita') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="date_of_birth" type="date"
+                                        class="form-control @error('date_of_birth') is-invalid @enderror"
+                                        name="date_of_birth" value="{{ old('date_of_birth') }}">
+
+                                    @error('date_of_birth')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="mb-4 row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Registrati') }}
+                                    </button>
+                                </div>
                             </div>
                         </div>
 
-                        <div class="mb-4 row">
-                            <label for="surname"
-                                class="col-md-4 col-form-label text-md-right">{{ __('Cognome') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="surname" type="text" pattern="[a-zA-Z\s]+" title="Solo lettere sono ammesse"
-                                    class="form-control @error('surname') is-invalid @enderror" name="surname"
-                                    value="{{ old('surname') }}" autocomplete="surname" autofocus>
-
-                                @error('surname')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="mb-4 row">
-                            <label for="date_of_birth"
-                                class="col-md-4 col-form-label text-md-right">{{ __('Data di nascita') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="date_of_birth" type="date"
-                                    class="form-control @error('date_of_birth') is-invalid @enderror"
-                                    name="date_of_birth" value="{{ old('date_of_birth') }}"
-                                    autocomplete="date_of_birth">
-
-                                @error('date_of_birth')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="mb-4 row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Registrati') }}
-                                </button>
-                            </div>
-                        </div>
                     </form>
                 </div>
             </div>
@@ -133,22 +136,21 @@
 @endsection
 
 <script>
-    document.addEventListener('DOMContentLoaded', function () {
-        const form = document.getElementById('register-form');
+    function validateEmail(event) {
+        event.preventDefault(); // Previene l'invio del modulo
+
         const emailInput = document.getElementById('email');
-        const emailFeedback = document.getElementById('email-feedback');
+        const emailValue = emailInput.value;
+        const emailPattern = /^[a-z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$/;
+        const emailError = document.getElementById("emailError");
 
-        form.addEventListener('submit', function (event) {
-            if (!emailInput.checkValidity()) {
-                event.preventDefault(); // Impedisce l'invio del modulo se l'email non è valida
-                emailFeedback.style.display = 'block'; // Mostra il messaggio di feedback
-            }
-        });
-
-        emailInput.addEventListener('input', function () {
-            if (emailInput.validity.valid) {
-                emailFeedback.style.display = 'none'; // Nasconde il messaggio di feedback se l'email diventa valida
-            }
-        });
-    });
+        if (!emailPattern.test(emailValue)) {
+            emailInput.classList.remove('error');
+            emailError.classList.remove('d-none');
+        } else {
+            emailInput.classList.remove('error');
+            // Se l'email è valida, puoi inviare il modulo
+            document.getElementById('register-form').submit();
+        }
+    }
 </script>
