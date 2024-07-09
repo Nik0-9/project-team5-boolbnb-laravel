1f46f0c4075d224464bce6fb5a80de15dffd6e80
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-07-09 20:39:16 +0200

    api.php da sistemare

--- routes/api.php
+++ routes/api.php
@@ -35,3 +35,58 @@ Route::post('apartments', [ApartmentController::class, 'store']);
 Route::post('/apartments/{apartment}/send-message', [MessageController::class, 'sendMessage']);
 Route::get('apartments/search/{address}/{latitude}/{longitude}', [ApartmentController::class, 'search']);
 Route::get('apartments/services-filter/{address}/{latitude}/{longitude}/{serviceIds?}', [ApartmentController::class, 'servicesSearch']);
+/*
+
+ <?php
+/*
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Api\ApartmentController;
+use App\Http\Controllers\Api\MessageController;
+
+Route::middleware('guest')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])
+                ->name('register');
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('apartments', [ApartmentController::class, 'index']);
+Route::get('apartments/sponsored', [ApartmentController::class, 'getSponsoredApartments']);
+Route::get('apartments/base', [ApartmentController::class, 'getBaseApartments']);
+Route::get('apartments/{slug}', [ApartmentController::class, 'show']);
+Route::post('apartments', [ApartmentController::class, 'store']);
+Route::put('apartments/{id}', [ApartmentController::class, 'update']);
+Route::delete('apartments/{id}', [ApartmentController::class, 'destroy']);
+Route::post('/apartments/{apartment}/send-message', [MessageController::class, 'sendMessage']); */
+/*use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Api\ApartmentController;
+use App\Http\Controllers\Api\MessageController;
+
+Route::post('api-register', [RegisteredUserController::class, 'store'])->name('api.register');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+Route::get('apartments', [ApartmentController::class, 'index']);
+Route::get('apartments/search/{address}/{lat}/{lon}', [ApartmentController::class, 'search']);
+Route::get('apartments/sponsored', [ApartmentController::class, 'sponsored']);
+Route::get('apartments/{id}', [ApartmentController::class, 'show']);
+Route::post('apartments', [ApartmentController::class, 'store']);
+Route::put('apartments/{id}', [ApartmentController::class, 'update']);
+Route::delete('apartments/{id}', [ApartmentController::class, 'destroy']);
+Route::post('/apartments/{apartment}/send-message', [MessageController::class, 'sendMessage']);
+Route::get('apartments/search/{address}/{latitude}/{longitude}', [ApartmentController::class, 'search']);
+Route::get('apartments/services-filter/{address}/{latitude}/{longitude}/{serviceIds?}', [ApartmentController::class, 'servicesSearch']);
+
+
+
+*/
