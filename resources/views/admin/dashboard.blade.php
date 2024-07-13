71aea64f3df26a0e2b30cf6255a032b8d503cbae
Nik0-9 <filannino.nicola9@gmail.com>
2024-07-13 19:22:15 +0200

    fix minor

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,28 +12,7 @@
                 @auth
                     <h2>{{ __('Benvenuto, :name!', ['name' => Auth::user()->name]) }}</h2>
                 @endauth
-                <h3>Ultimi messaggi</h3>
-                @foreach ($apartments as $apartment)
-                    <div class="card">
-                        <div class="card-body d-flex justify-content-between align-items-center">
-                            <div class="d-flex flex-column">
-                                <h5 class="card-title">{{ $apartment->name }}</h5>
-                                <p class="card-text">Nuovi Messaggi:{{ $apartment->messages_count }}</p>
-                            </div>
-                            <div>
-                                <img src="{{ asset('storage/' . $apartment->cover_image)}}" alt="{{ $apartment->name }}" style="width: 200px; height: 150px">
-                            </div>
-                        </div>
-                        <div class="d-flex p-2 gap-2">    
-                            <a href="{{ route('admin.apartments.messages', $apartment->slug) }}" class="btn btn-admin w-50">Visualizza Messaggi</a>
-                            <a href="{{ route('admin.apartments.show', $apartment->slug) }}" class="btn btn-admin w-50 ">Vedi Appartamento</a>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-            
-            
-            <div class="col-12 col-xl-6">
+                <div class="col-12 col-xl-6">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Totale Messaggi Ricevuti</h5>
@@ -41,9 +20,30 @@
                     </div>
                 </div>
             </div>
-        </div>
+                <h3>Ultimi messaggi</h3>
+                @foreach ($apartments as $apartment)
+                    <div class="card mb-2">
+                        <div class="card-body d-flex justify-content-between align-items-center">
+                            <div class="d-flex flex-column">
+                                <h5 class="card-title">{{ $apartment->name }}</h5>
+                                <p class="card-text">Nuovi Messaggi:{{ $apartment->messages_count }}</p>
+                            </div>
+                            <div>
+                                <img src="{{ asset('storage/' . $apartment->cover_image)}}" alt="{{ $apartment->name }}"
+                                    style="width: 200px; height: 150px">
+                            </div>
+                        </div>
+                        <div class="d-flex p-2 gap-2">
+                            <a href="{{ route('admin.apartments.messages', $apartment->slug) }}"
+                                class="btn btn-admin w-50">Visualizza Messaggi</a>
+                            <a href="{{ route('admin.apartments.show', $apartment->slug) }}"
+                                class="btn btn-admin w-50 ">Vedi Appartamento</a>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 </div>
+</div>
 @endsection
-
