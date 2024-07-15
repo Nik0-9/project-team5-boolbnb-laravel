0f7ad67f1ed57e056ee2b5e2923b7f97cfd35899
Nik0-9 <filannino.nicola9@gmail.com>
2024-07-15 21:49:17 +0200

    fix

--- resources/views/admin/partials/modal-delete.blade.php
+++ resources/views/admin/partials/modal-delete.blade.php
@@ -3,11 +3,11 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="deleteModalLabel">Atenzione!!!</h1>
+                <h1 class="modal-title fs-5" id="deleteModalLabel">Attenzione!!!</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <p>Sei sicuro di voler cancellare questo appartamento <strong>{{ $apartment->name}}</strong> <span id="modal-item-title"></span>?</p>
+                <p>Sei sicuro di voler cancellare l'appartamento <strong>{{ $apartment->name}}?</strong> <span id="modal-item-title"></span></p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
